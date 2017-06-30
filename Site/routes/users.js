526d54549f8f614f6a14c63da5f6834e496a0a39
Ady Moussa <adymoussa.edu@gmail.com>
2017-06-30 08:48:26 +0200

    Users - Hardcoded users JSON before using the DB

--- Site/routes/users.js
+++ Site/routes/users.js
@@ -7,7 +7,21 @@ module.exports = function (app) {
 	// :: Routes
 
 	app.get ("/users/:name", function (request, response, next) {
-		response.send (request.params);
+		// response.send (request.params);
+
+		response.send ([
+			{
+				"username" : "amo",
+				"fornavn"  : "Ady",
+				"efternavn": "Moussa"
+			},
+			{
+				"username" : "fm",
+				"fornavn"  : "Funky",
+				"efternavn": "Monkey"
+			}
+		]);
+
 	});
 
 };
